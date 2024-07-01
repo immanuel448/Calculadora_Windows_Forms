55a9ce87dc85dc38745da30462711b377de015dd
Emmanuel Mendoza <emmanuelmendoza448@gmail.com>
2024-06-30 19:09:26 -0600

    segunda branch

--- calculadora002/ParteLogica.cs
+++ calculadora002/ParteLogica.cs
@@ -13,51 +13,6 @@ namespace calculadora002
             return dato < 10000000000 && dato > -10000000000 ? true : false;
         }
 
-        public bool sEvaluarRangoNumero(double doubleIngresado)
-        {
-            // Verificar si está dentro del rango
-            if (doubleIngresado > -10000000000 && doubleIngresado < 10000000000)
-            {
-                //DENTRO DEL RANGO
-                // Convertir el número a string para verificar los ceros después del punto decimal
-                string datoString = doubleIngresado.ToString();
-
-                // Buscar el punto decimal
-                int puntoIndex = datoString.IndexOf('.');
-                Console.WriteLine("dentro de rango");
-                if (puntoIndex != -1 && (datoString.Length >= puntoIndex + 1))
-                {
-                    Console.WriteLine("dentro de rango y con punto");
-                    //si hay punto y hay almenos un decimal
-                    string parteDecimal = datoString.Substring(puntoIndex + 1);
-                    if (parteDecimal.Length >= 8)
-                    {
-                        Console.WriteLine("dentro de rango con mas de 8 decimales");
-                        //mínimo 8 decimales
-                        //recorrer el string
-                        for (int i = 0; i < parteDecimal.Length; i++)
-                        {
-                            if (parteDecimal[i] != '0')
-                            {
-                                //mínimo uno no es cero
-                                return true;
-                            }
-                            else
-                            {
-                                //Son mínimo 9 ceros, NO PASA
-                                return false;
-                            }
-                        }
-                    }
-                    return true; // menor a 9 decimales es ok
-                }
-
-                return true; // Está dentro del rango y cumple con la condición de ceros después del punto
-            }
-
-            return false; // No está dentro del rango
-        }
-
         //este método sólo cuenta la parte entera
         public bool EvaluarDimensionString(string numeroString)
         {
